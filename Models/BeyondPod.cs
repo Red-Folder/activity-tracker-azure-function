98a3aab8e9ff4c2a7587713e8767a71356685d63
Mark Taylor <mark.taylor@Betfred.com>
2018-09-13 17:42:22 +0100

    Partial conversion to c#

---
+++ Models/BeyondPod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Red_Folder.ActivityTracker.Models
+{
+    public class BeyondPod
+    {
+        public DateTime Created { get; set; }
+        public bool Playing { get; set; }
+        public string FeedName { get; set; }
+        public string FeedUrl { get; set; }
+        public string EpisodeName { get; set; }
+        public string EpisodeUrl { get; set; }
+        public string EpisodeFile { get; set; }
+        public string EpisodePostUrl { get; set; }
+        public string EpisodeMime { get; set; }
+        public string EpisodeSummary { get; set; }
+        public long EpisodeDuration { get; set; }
+        public long EpisodePosition { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Track { get; set; }
+    }
+}
---
+++ Models/PodCastTableEntity.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Red_Folder.ActivityTracker.Models
+{
+    public class PodCastTableEntity : TableEntity
+    {
+        public PodCastTableEntity() : base()
+        {
+        }
+
+        public PodCastTableEntity(string partitionKey, string rowKey) : base(partitionKey, rowKey)
+        {
+        }
+
+        public DateTime Created { get; set; }
+        public bool Playing { get; set; }
+        public string FeedName { get; set; }
+        public string FeedUrl { get; set; }
+        public string EpisodeName { get; set; }
+        public string EpisodeUrl { get; set; }
+        public string EpisodeFile { get; set; }
+        public string EpisodePostUrl { get; set; }
+        public string EpisodeMime { get; set; }
+        public string EpisodeSummary { get; set; }
+        public long EpisodeDuration { get; set; }
+        public long EpisodePosition { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Track { get; set; }
+
+        public bool Actioned { get; set; }
+
+        public PodCast ToPodCast()
+        {
+            throw new NotImplementedException("TODO");
+
+            return new PodCast
+            {
+                Created = this.Created,
+                Playing = this.Playing,
+                FeedName = this.FeedName,
+                FeedUrl = this.FeedUrl,
+                EpisodeName = this.EpisodeName,
+                EpisodeUrl = this.EpisodeUrl
+
+            };
+        }
+    }
+}
