9847ceb4098f168e973ca201ed440d9dc868abf9
Red Folder <markbryantaylor@yahoo.co.uk>
2018-12-27 10:00:38 +0000

    Added json formatting for the weekly activity to be tweeted model

--- Models/WeeklyActivityToBeTweeted.cs
+++ Models/WeeklyActivityToBeTweeted.cs
@@ -1,9 +1,14 @@
-﻿namespace Red_Folder.ActivityTracker.Models
+﻿using Newtonsoft.Json;
+
+namespace Red_Folder.ActivityTracker.Models
 {
     public class WeeklyActivityToBeTweeted
     {
+        [JsonProperty("year")]
         public int Year { get; set; }
+        [JsonProperty("weekNumber")]
         public int WeekNumber { get; set; }
+        [JsonProperty("imageUrl")]
         public string ImageUrl { get; set; }
     }
 }
