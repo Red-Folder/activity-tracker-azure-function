e926484cb3847d3fd367ca01a48dd371b007dc3d
Mark Taylor <mark.taylor@Betfred.com>
2019-03-18 11:39:30 +0000

    Added events to the weekly activity model

---
+++ Models/Event.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace Red_Folder.ActivityTracker.Models
+{
+    public class Event
+    {
+        [JsonProperty("imageUrl")]
+        public string ImageUrl { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
