6f099dd7df28fbe71f8c2ef7cc42b99d99741c21
Mark Taylor <mark.taylor@Betfred.com>
2019-05-09 14:08:00 +0100

    Added failing test to prove CI

---
+++ RedFolder.ActivityTracker.ActivityBot.UnitTests/TemporaryTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RedFolder.ActivityTracker.ActivityBot.UnitTests
+{
+
+    public class TemporaryTest
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(false);
+        }
+    }
+}
