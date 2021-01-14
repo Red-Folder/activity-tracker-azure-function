4ffc6078ac93a68d45f981e1b7369ad6b0b20a1f
Red Folder <markbryantaylor@yahoo.co.uk>
2021-01-14 14:34:52 +0000

    Start of unit testing the week logic

---
+++ RedFolder.ActivityTracker.UnitTests/Models/WeekUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using RedFolder.ActivityTracker.Models;
+using System;
+using Xunit;
+
+namespace RedFolder.ActivityTracker.UnitTests.Models
+{
+    public class WeekUnitTests
+    {
+        [Theory]
+        [InlineData("2021-01-11", "2021-01-04")]
+        public void FromDate_CorrectCalculatesStartDate(string seedDate, string expected)
+        {
+            var sut = Week.FromDate(DateTime.Parse(seedDate).AddDays(-7));
+            Assert.Equal(DateTime.Parse(expected), sut.Start);
+        }
+    }
+}
